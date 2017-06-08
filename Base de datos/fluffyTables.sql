6b4de8e8b01792650aed187b91bf139c3f1373d5
berto2904 <berto2904@gmail.com>
2017-06-08 12:23:25 -0300

    Update del script sql
    Chicos, les dejo actualizado el script para que cargar los datos a las
    tablas

--- Base de datos/fluffyTables.sql
+++ Base de datos/fluffyTables.sql
@@ -218,6 +218,44 @@ COMMIT;
 
 
 -- -----------------------------------------------------
+-- Data for table `sexo`
+-- -----------------------------------------------------
+START TRANSACTION;
+USE `fluffy`;
+INSERT INTO `sexo` (`id_sexo`, `descripcion`) VALUES (1, 'Masculino');
+INSERT INTO `sexo` (`id_sexo`, `descripcion`) VALUES (2, 'Femenino');
+INSERT INTO `sexo` (`id_sexo`, `descripcion`) VALUES (3, 'Macho');
+INSERT INTO `sexo` (`id_sexo`, `descripcion`) VALUES (4, 'Hembra');
+
+COMMIT;
+
+
+-- -----------------------------------------------------
+-- Data for table `usuario`
+-- -----------------------------------------------------
+START TRANSACTION;
+USE `fluffy`;
+INSERT INTO `usuario` (`id_usuario`, `nombre`, `id_ubicacion`, `id_sexo`, `telefono`, `id_domicilio`, `e_mail`, `fecha_nacimiento`, `ultima_conexion`, `contrasenia`, `nombre_usuario`, `apellido`) VALUES (1, 'nombre', NULL, 1, NULL, NULL, 'pruebaMail1', NULL, NULL, '1234', 'user1', 'berto');
+INSERT INTO `usuario` (`id_usuario`, `nombre`, `id_ubicacion`, `id_sexo`, `telefono`, `id_domicilio`, `e_mail`, `fecha_nacimiento`, `ultima_conexion`, `contrasenia`, `nombre_usuario`, `apellido`) VALUES (2, 'nombre2', NULL, 2, NULL, NULL, 'pruebaMail2', NULL, NULL, '1234', 'user2', 'bertoli');
+INSERT INTO `usuario` (`id_usuario`, `nombre`, `id_ubicacion`, `id_sexo`, `telefono`, `id_domicilio`, `e_mail`, `fecha_nacimiento`, `ultima_conexion`, `contrasenia`, `nombre_usuario`, `apellido`) VALUES (3, 'nombre3', NULL, 1, NULL, NULL, 'pruebaMail3', NULL, NULL, '1234', 'user3', 'sarasa');
+INSERT INTO `usuario` (`id_usuario`, `nombre`, `id_ubicacion`, `id_sexo`, `telefono`, `id_domicilio`, `e_mail`, `fecha_nacimiento`, `ultima_conexion`, `contrasenia`, `nombre_usuario`, `apellido`) VALUES (4, 'nombre4', NULL, 2, NULL, NULL, 'pruebaMail4', NULL, NULL, '1234', 'user4', 'pepito');
+
+COMMIT;
+
+
+-- -----------------------------------------------------
+-- Data for table `muro_mascota`
+-- -----------------------------------------------------
+START TRANSACTION;
+USE `fluffy`;
+INSERT INTO `muro_mascota` (`id_muro_mascota`, `adopcion`, `cita`, `perdido`) VALUES (1, 0, 0, 0);
+INSERT INTO `muro_mascota` (`id_muro_mascota`, `adopcion`, `cita`, `perdido`) VALUES (2, 0, 0, 0);
+INSERT INTO `muro_mascota` (`id_muro_mascota`, `adopcion`, `cita`, `perdido`) VALUES (3, 0, 0, 0);
+
+COMMIT;
+
+
+-- -----------------------------------------------------
 -- Data for table `animal`
 -- -----------------------------------------------------
 START TRANSACTION;
@@ -236,5 +274,52 @@ USE `fluffy`;
 INSERT INTO `raza` (`id_raza`, `id_animal`, `descripcion`) VALUES (1, 1, 'Sin Raza');
 INSERT INTO `raza` (`id_raza`, `id_animal`, `descripcion`) VALUES (2, 2, 'Sin Raza');
 INSERT INTO `raza` (`id_raza`, `id_animal`, `descripcion`) VALUES (100, 1, 'Ovejero Aleman');
+
+COMMIT;
+
+
+-- -----------------------------------------------------
+-- Data for table `mascota`
+-- -----------------------------------------------------
+START TRANSACTION;
+USE `fluffy`;
+INSERT INTO `mascota` (`id_mascota`, `id_usuario`, `id_sexo`, `fecha_nacimiento`, `url_lite`, `nombre`, `id_muro_mascota`, `id_raza`, `id_animal`) VALUES (1, 1, 3, NULL, NULL, 'nombreMascota1', 1, 1, 1);
+INSERT INTO `mascota` (`id_mascota`, `id_usuario`, `id_sexo`, `fecha_nacimiento`, `url_lite`, `nombre`, `id_muro_mascota`, `id_raza`, `id_animal`) VALUES (2, 1, 4, NULL, NULL, 'nombreMascota2', 2, 2, 2);
+INSERT INTO `mascota` (`id_mascota`, `id_usuario`, `id_sexo`, `fecha_nacimiento`, `url_lite`, `nombre`, `id_muro_mascota`, `id_raza`, `id_animal`) VALUES (1, 2, 3, NULL, NULL, 'Chatran', 3, 1, 1);
+
+COMMIT;
+
+
+-- -----------------------------------------------------
+-- Data for table `experiencia`
+-- -----------------------------------------------------
+START TRANSACTION;
+USE `fluffy`;
+INSERT INTO `experiencia` (`id_experiencia`, `id_muro_mascota`, `fecha_experiencia`, `foto_experiencia`, `video_experiencia`, `comentario_experiencia`, `valoracion`) VALUES (1, 1, '2017-06-08 12:00', NULL, NULL, 'Comentario de prueba', 1);
+INSERT INTO `experiencia` (`id_experiencia`, `id_muro_mascota`, `fecha_experiencia`, `foto_experiencia`, `video_experiencia`, `comentario_experiencia`, `valoracion`) VALUES (2, 1, '2017-06-08 12:00', NULL, NULL, 'Comentario de prueba2', 1);
+INSERT INTO `experiencia` (`id_experiencia`, `id_muro_mascota`, `fecha_experiencia`, `foto_experiencia`, `video_experiencia`, `comentario_experiencia`, `valoracion`) VALUES (3, 2, '2017-06-08 12:00', NULL, NULL, 'Comentario de prueba3', 1);
+
+COMMIT;
+
+
+-- -----------------------------------------------------
+-- Data for table `comentario_externo`
+-- -----------------------------------------------------
+START TRANSACTION;
+USE `fluffy`;
+INSERT INTO `comentario_externo` (`id_comentario_externo`, `id_usuario`, `id_experiencia`, `comentario`) VALUES (1, 2, 1, 'Yo hago un comentario loco a lo chatran');
+INSERT INTO `comentario_externo` (`id_comentario_externo`, `id_usuario`, `id_experiencia`, `comentario`) VALUES (2, 2, 1, 'Hago otro comentario xq estoy re loco');
+INSERT INTO `comentario_externo` (`id_comentario_externo`, `id_usuario`, `id_experiencia`, `comentario`) VALUES (3, 3, 1, 'Yo soy otro usuario comentando cosas locas');
+INSERT INTO `comentario_externo` (`id_comentario_externo`, `id_usuario`, `id_experiencia`, `comentario`) VALUES (4, 1, 1, 'puto el que lee');
+
+COMMIT;
+
+
+-- -----------------------------------------------------
+-- Data for table `seguimiento`
+-- -----------------------------------------------------
+START TRANSACTION;
+USE `fluffy`;
+INSERT INTO `seguimiento` (`id_usuario`, `id_mascota`, `id_usuario_mascota`, `fecha_seguimiento`) VALUES (2, 1, 1, '2017-06-08');
 
 COMMIT;
