fece17fa7cb7c13c2f7a6ece85258d9988776930
demianbacigalupo <mercenary666_12@hotmail.com>
2017-06-17 13:55:45 -0300

    Creado boton modal

--- vistas/pantallaLogueada.php
+++ vistas/pantallaLogueada.php
@@ -25,5 +25,25 @@ else {
 
     <input type="button" name="" value="agregarMascota" onclick="agregarMascota(<?= $id_usuario ?>)">
 
+    <a data-toggle="modal" href="#example" class="btn btn-primary btn-large">Abrir ventana modal</a>
+
+    <div id="example" class="modal hide fade in" style="display: none;">
+    <div class="modal-header">
+        <a data-dismiss="modal" class="close">×</a>
+        <h3>Cabecera de la ventana</h3>
+     </div>
+     <div class="modal-body">
+         <h4>Texto de la ventana</h4>
+        <p>Mas texto en la ventana.</p>                
+    </div>
+    <div class="modal-footer">
+        <a href="index.html" class="btn btn-success">Guardar</a>
+        <a href="#" data-dismiss="modal" class="btn">Cerrar</a>
+    </div>
+</div>
+
+  <script src="jquery.js"></script>
+  <script src="bootstrap-modal.js"></script>
+
   </body>
 </html>
