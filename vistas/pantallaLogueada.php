0074bd1ab97fd3b9a7ba4bbea0e99bb423567165
vegitojor <j.velazquez1985@gmail.com>
2017-06-25 14:21:43 -0300

    cambios en pantalla logueada

--- vistas/pantallaLogueada.php
+++ vistas/pantallaLogueada.php
@@ -13,7 +13,7 @@ else {
 }
 ?>
 <html>
-  <head>
+  <head ng-app="fluffy">
     <meta charset="utf-8">
     <script src="../librerias/jquery-3.1.1.js" charset="utf-8" type="text/javascript"></script>
     <script src="../js/home.js" charset="utf-8" type="text/javascript"></script>
@@ -22,6 +22,34 @@ else {
   <body>
     HOLAAAA SOY UNA PANTALLA LOGUEADA
     <input type="button" name="" value="mostrarHistorias" onclick="mostrarUltimasHistorias()">
+
+
+    <!-- MODAL PARA EDITAR DATOS DEL USUARIO -->
+    <div id="myModal" class="modal fade" role="dialog">
+      <div class="modal-dialog">
+
+        <!-- Modal content -->
+        <div class="modal-content">
+          <div class="modal-header">
+            <button type="button" class="close" data-dismiss="modal">&times;</button>
+            <h4 class="modal-title">Editar perfil de usuario</h4>
+          </div>
+          <div class="modal-body">
+            <form ng-modal="usuario">
+              <div class="form-group">
+                <label for="text">Nuevo nombre</label>
+                <input type="text" class="form-control" id="nuevoNombre" value="{{usuario.nombre}}">
+              </div>
+            </form>
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    
 
   </body>
 </html>
