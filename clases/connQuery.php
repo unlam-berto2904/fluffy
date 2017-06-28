cc0f0b046e9a95751c11823666868f50dd976448
berto2904 <berto2904@gmail.com>
2017-06-28 02:11:10 -0300

    pass

--- clases/connQuery.php
+++ clases/connQuery.php
@@ -3,7 +3,7 @@ class ConnQuery{
 
   private $servidor ="localhost";
   private $usuario = "root";
-  private $pass = "digio";
+  private $pass = "admin2904";
   private $bd="fluffy";
   private $conn;
 
