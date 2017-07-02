c833e18817f7b1efa7b9c73bf79e4615ef0893d0
berto2904 <berto2904@gmail.com>
2017-07-02 17:45:14 -0300

    Agergar js de ranking mascotas

--- vistas/home.php
+++ vistas/home.php
@@ -305,44 +305,7 @@ else {
     <script src="../js/vistaExperiencia.js" charset="utf-8" types="text/javascript"></script>
     <script src="../js/menu_home.js" type="text/javascript"></script>
     <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
-    <script type="text/javascript">
-    $("#menu-toggle").click(function(e) {
-        e.preventDefault();
-        $("#wrapper").toggleClass("toggled");
-    });
-    // Load the Visualization API and the corechart package.
-    google.charts.load('current', {'packages':['corechart']});
-
-    // Set a callback to run when the Google Visualization API is loaded.
-    google.charts.setOnLoadCallback(drawChart);
-
-    // Callback that creates and populates a data table,
-    // instantiates the pie chart, passes in the data and
-    // draws it.
-    function drawChart() {
-
-      // Create the data table.
-      var data = new google.visualization.DataTable();
-      data.addColumn('string', 'Topping');
-      data.addColumn('number', 'Slices');
-      data.addRows([
-        ['Mushrooms', 3],
-        ['Onions', 1],
-        ['Olives', 1],
-        ['Zucchini', 1],
-        ['Pepperoni', 2]
-      ]);
-
-      // Set chart options
-      var options = {'title':'How Much Pizza I Ate Last Night',
-      'width':400,
-      'height':300};
-
-      // Instantiate and draw our chart, passing in some options.
-      var chart = new google.visualization.ColumnChart(document.getElementById('prueba'));
-      chart.draw(data, options);
-    }
-    </script>
+    <script src="../js/rankingDeMascotas.js" charset="utf-8" type="text/javascript"> </script>
     <script src="../js/ajaxCargaMascota.js" type="text/javascript" ></script>
     <script src="../librerias/bootstrap/js/bootstrap.min.js"></script>
   </body>
