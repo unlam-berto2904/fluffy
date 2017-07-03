806f4df8401c4d54b0abb250b87d2d9d68f4b92f
berto2904 <berto2904@gmail.com>
2017-07-03 16:40:38 -0300

    Listo! Funcionalmente mergeado! falta QR

--- clases/connQuery.php
+++ clases/connQuery.php
@@ -3,14 +3,14 @@ class ConnQuery{
 
   private $servidor ="localhost";
   private $usuario = "root";
-  private $pass = "abrh++++";
+  private $pass = "admin2904";
   private $bd="fluffy";
   private $conn;
 
 
   function __construct(){
     $this->conn = mysqli_connect($this->servidor, $this->usuario, $this->pass, $this->bd);
-    
+
   }
 
   function ejecutarConsultaIsTrue($sql){
